966773db1f5efc2cf473bbba7a78a596ca4a939c
AndrewMatsievsky <strymok89@gmail.com>
2016-08-14 19:26:47 +0300

    added webpack

---
+++ index.js
@@ -0,0 +1,1 @@
+require("./public/style/main.css");
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var config = {
+   entry: './public/main.js',
+	
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      host: "192.168.1.3",
+      port: 8081
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         },
+	{ test: /\.css$/, loader: "style-loader!css-loader" }
+      ]
+   }
+}
+
+module.exports = config;
